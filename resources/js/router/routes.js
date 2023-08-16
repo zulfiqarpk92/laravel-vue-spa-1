cretueusebiu/laravel-vue-spa

Supplier and Employer
pages of list, add and edit/update is completed. Have issue in employee edit page. Hope you can check and solve.

resources/js/router/routes.js
@@ -18,7 +18,7 @@ export default [
   { path: '/customers/add', name: 'customers.add', component: page('customers/CustomerForm.vue') },
   { path: '/customers/edit/:customerId', name: 'customers.edit', component: page('customers/CustomerDetail.vue'), props: true },
 
-  { path: '/employees', name: 'employees', component: page('employees/employees.vue') },
+  { path: '/employees', name: 'employees', component: page('employees/Index.vue') },
   { path: '/employees/add', name: 'employees.add', component: page('employees/employeeform.vue') },
   { path: '/employees/edit/:employeeId', name: 'employees.edit', component: page('employees/employeeform.vue'), props: true },
 
@@ -33,6 +33,7 @@ export default [
 
   { path: '/suppliers', name: 'suppliers', component: page('suppliers/Suppliers.vue') },
   { path: '/suppliers/add', name: 'suppliers.add', component: page('suppliers/supplierform.vue') },
+  { path: '/suppliers/edit/:supplierId', name: 'suppliers.edit', component: page('suppliers/supplierform.vue'), props: true },
   {
     path: '/settings',
     component: page('settings/index.vue'),

zulfiqarpk92/laravel-vue-spa-1

resources/js/router/routes.js
@@ -18,7 +18,7 @@ export default [
   { path: '/customers/add', name: 'customers.add', component: page('customers/CustomerForm.vue') },
   { path: '/customers/edit/:customerId', name: 'customers.edit', component: page('customers/CustomerDetail.vue'), props: true },
 
-  { path: '/employees', name: 'employees', component: page('employees/employees.vue') },
+  { path: '/employees', name: 'employees', component: page('employees/Index.vue') },
   { path: '/employees/add', name: 'employees.add', component: page('employees/employeeform.vue') },
   { path: '/employees/edit/:employeeId', name: 'employees.edit', component: page('employees/employeeform.vue'), props: true },
 
@@ -33,6 +33,7 @@ export default [
 
   { path: '/suppliers', name: 'suppliers', component: page('suppliers/Suppliers.vue') },
   { path: '/suppliers/add', name: 'suppliers.add', component: page('suppliers/supplierform.vue') },
+  { path: '/suppliers/edit/:supplierId', name: 'suppliers.edit', component: page('suppliers/Invoice.vue'), props: true },
   {
     path: '/settings',
     component: page('settings/index.vue'),